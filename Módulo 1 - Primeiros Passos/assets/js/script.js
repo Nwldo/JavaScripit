f1ab049c05a7a7ae4085e9adbd45ff51fb762cc1
Nwldo <regenildoliveira@yahoo.com.br>
2024-01-21 20:29:34 -0300

    Inciando o curso de JavaScript

---
+++ Módulo 1 - Primeiros Passos/assets/js/script.js
@@ -0,0 +1,22 @@
+/*alert('Olá mundo!')*/
+
+/* não mostra nada na tela e sim no console */
+console.log('ola mundo 3.0')
+
+/* Variávies */
+let idade = 90
+let nome = 'nwldo'
+
+console.log(idade)
+console.log(nome)
+
+/* Tipos de dados */
+let x = 90 // numero
+let name = 'nwldo' // string
+let logado = true // boolean
+let lista = ["a","b"] // array
+let nomeCompleto = {nome: "Nwldo", idade: 30} // objeto
+
+console.log(typeof nome)
+console.log(typeof logado)
+console.log(typeof nomeCompleto)
