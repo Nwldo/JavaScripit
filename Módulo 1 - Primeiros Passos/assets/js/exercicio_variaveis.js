d6411b4fbb928fcf0d33d40db52a492e75992256
Nwldo <regenildoliveira@yahoo.com.br>
2024-01-21 21:56:35 -0300

    Módulo 1 - aula 10: let, const, var

---
+++ Módulo 1 - Primeiros Passos/assets/js/exercicio_variaveis.js
@@ -0,0 +1,14 @@
+// 1. Crie uma variável para armazenar o nome
+// do carro e ponha o valor Ferrai. E exiba-a.
+let nomeCarro = "Ferrari"
+console.log(nomeCarro)
+
+// 2. Crie uma vaiável paa armazenar o preço
+// do bolo, que custa R$ 19,99. E exiba-a
+let prcoBolo = 19.99
+console.log(prcoBolo)
+
+// 3. Corrija o código a seguir (e xiba-o).
+// let "cidade" = Teresina
+let cidade = "Teresina"
+console.log(cidade)
--- Módulo 1 - Primeiros Passos/assets/js/script.js
+++ Módulo 1 - Primeiros Passos/assets/js/script.js
@@ -1,6 +1,6 @@
 /*alert('Olá mundo!')*/
 
-/* não mostra nada na tela e sim no console */
+/* o comando abaixo exibe os dados no console */
 console.log('ola mundo 3.0')
 
 /* Variávies */
@@ -20,3 +20,32 @@ let nomeCompleto = {nome: "Nwldo", idade: 30} // objeto
 console.log(typeof nome)
 console.log(typeof logado)
 console.log(typeof nomeCompleto)
+
+/* Ponto e vírgula */
+// Opcional - executar um único código na linha
+let teste = "teste"
+
+// Obrigatório - executar mais de um código na linha
+// para definir o fim do código
+let obrigatorio = "teste 1"; let numero = 1.0
+
+
+
+// let, const, var
+let nome1 = "Nwldo" // var funciona da mesma forma
+console.log(nome1)
+
+nome1 = "João"
+console.log(nome1)
+
+// var funciona em qualquer contexto
+// let funciona apenas no contexto que foi criada
+
+const nome2 = "Pedro" // valor que não muda
+console.log(nome2)
+
+nome2 = "Maria"
+console.log(nome2)
+
+
+
