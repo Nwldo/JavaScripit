9fd15ddb71b1ce2493e63e7464ef6fe827071760
Nwldo <regenildoliveira@yahoo.com.br>
2024-01-30 12:12:26 -0300

    Continuando o módulo 3 - manipulando elementos

--- Módulo 3 - Eventos e manipulação de DOM/script.js
+++ Módulo 3 - Eventos e manipulação de DOM/script.js
@@ -39,7 +39,77 @@ function clicar() {
 
     ul.outerHTML = "<strong>Alterado!</strong>"
 
-    
 }
+
+/* Modificar elementos 2
+ Adicionar elementos usa-se o 
+
+
+*/
+
+function modificar() {
+    const lista1 = document.querySelector('#lista1');
+    const ul = lista1.querySelector('ul');
+
+    //ul.children[0].innerHTML += "(alterado)";
+    //ul.children[0].append("(alterado)");
+
+    let newLi = document.createElement('li');
+    newLi.innerText = "Intem adicionado";
+
+    ul.appendChild(newLi);
+
+}
+
+/**
+A diferença do innerHTML para o append é no processamento, pois o primeiro remove tudo do memório e
+adicionar novamente o valor anterior mais conteúdo novo, já o segundo só adiciona o conteúdo novo.
+
+Obs.: só funciona para textos. --- para adicionar um elemento usa-se appendChild (adiciona no final)
+mas antes tem que criar o elemento na memória
+
+o prepend(nwLi) adicionar no inicio
+
+ */
+
+//Manipulando elementos 3 - adicionando elementos antes e depois
+// AFTER --> depois
+// BEFORE --> ANTES
+
+function adicionar() {
+    const lista3 = document.querySelector('#lista3');
+    const ul = lista3.querySelector('ul');
+
+    //ul.after("Texto depois.....");
+    //ul.before("Texto antes..");
+
+    /*
+    const newButton = document.createElement('button');
+    newButton.innerHTML = "Botão";
+
+    ul.before(newButton);
+    */
+
+    // Adicionar uma nova lista
+    newUl = document.querySelector('ul');
+    //ul.after(newUl); pode ficar antes
+
+    // adicionar dentro dele
+    //newLi = document.querySelector('li');
+    //newUl.append(newLi);
+    //com o loop
+    for(let i = 0; i < 5; i++) {
+        let newLi = document.createElement('li');
+        newLi.innerHTML = "Item add " + i;
+        newUl.append(newLi);
+    }
+
+    ul.after(newUl);
+}
+
+// para adicionar um elementos em vez de texto
+// cria elemento ou pegar o elemento do outro lugar
+
+
 
 
